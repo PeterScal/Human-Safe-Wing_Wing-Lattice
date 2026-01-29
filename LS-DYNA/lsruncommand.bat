b251fd881a8d9865095f5e27da880eff391bdcdd
PeterScal <peterscal26@gmail.com>
2026-01-29 13:17:27 -0500

    next commit

--- LS-DYNA/lsruncommand.bat
+++ LS-DYNA/lsruncommand.bat
@@ -1,4 +1,4 @@
-"C:\Program Files\LS-DYNA Suite R16.1 Student\lsdyna\ls-dyna_smp_d_R16.1_180-gd50332dbe5_winx64_ifort190_sse2_studentversion.exe" i=D:\Modified_Wing\LS-DYNA\WingLattice_System.k ncpu=4 memory=34m
+"C:\Program Files\LS-DYNA Suite R16.1 Student\lsdyna\ls-dyna_smp_d_R16.1_180-gd50332dbe5_winx64_ifort190_sse2_studentversion.exe" i=D:\Modified_Wing\LS-DYNA\WingLattice_System.k ncpu=4 memory=20m
 @echo off
-echo job finished > lsrunjobid11
+echo job finished > lsrunjobid3
 pause
